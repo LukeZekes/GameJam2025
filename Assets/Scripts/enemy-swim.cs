0af75a4fca487077c1b8476fafd17ee85c9a51b7
KitCatLion <91389279+KitCatLion@users.noreply.github.com>
2025-01-24 23:27:47 -0500

    Enemy movements

---
+++ Assets/Scripts/enemy-swim.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public GameObject Fish;
+    public GameObject Shark;
+    public GameObject Eel;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
