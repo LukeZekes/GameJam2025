8ed395565d143cabe5441157af2bf24a44131c81
Vedasri Malatker <vedasri.malatker@gmail.com>
2025-01-25 18:08:53 -0500

    Animator Update

--- Assets/Scripts/CharaANIMEManager.cs
+++ Assets/Scripts/CharaANIMEManager.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharaANIMEManager : MonoBehaviour
 {
+    Animator charaManager;
+
+    InputAction jump;
+    InputAction move;
+    InputAction dash;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        charaManager = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerAttack.)
     }
 }
