4635aef9b38bbb1f86a6d7577b99a0caa3e8682a
LukeZekes <zechesl@gmail.com>
2025-01-24 22:42:10 -0600

    Use cinemachine and remove temp movement

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -5,25 +5,18 @@ public class PlayerCamera : MonoBehaviour
     private Camera _camera;
     public float leftBoundary;
     public float rightBoundary;
+    public float stageBottom;
+    public float stageTop;
 
-    void Awake()
-    {
-        _camera = GetComponentInChildren<Camera>();
-    }
     void Start()
     {
-        _camera.transform.position = new Vector3(0, 0, -10);
+        _camera = Camera.main;
         leftBoundary = _camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
         rightBoundary = _camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x;
     }
 
     void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float moveSpeed = 5f;
-        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
-        
-        
+    {        
         // If the player is outside the boundary, teleport them to the other side of the map
         if (transform.position.x < leftBoundary)
         {
