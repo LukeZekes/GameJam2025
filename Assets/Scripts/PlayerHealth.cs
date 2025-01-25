336b048217913e0f2ffb388b9e1ed8e0589b4efb
Adam Palmer <Dusklynx44@gmail.com>
2025-01-25 15:05:24 -0500

    File Naming Fixes

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+//Vedasri Malatker
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //Game Objects
+    GameObject player;
+
+    //Private Variables
+    private int health = 30;
+
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health == 0)
+        {
+            //Game Over
+        }
+    }
+
+    void TakeDamage(int dmg)
+    {
+        if (health == 0)
+        {
+            //Game Over
+        }
+        
+        health = (health - dmg);
+    }
+}
