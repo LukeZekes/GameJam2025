844a07bf38bf2448fe5b277d0cde08036d9c48c4
LukeZekes <zechesl@gmail.com>
2025-01-26 15:02:54 -0600

    You win voice

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,27 +1,42 @@
+using System.Collections;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] AudioClip winTheme, loseTheme, mainTheme;
-    AudioSource audioSource;
+    AudioSource audioSource, voiceAudioSource;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        voiceAudioSource = transform.GetChild(0).GetComponent<AudioSource>();
     }
 
     public void PlayWinTheme() {
         if (winTheme == null) Debug.LogWarning("Win Theme not set in Audio Manager on GameManager");
+        audioSource.Stop();
+        audioSource.loop = false;
         audioSource.clip = winTheme;
+        StartCoroutine(PlayVoiceWithDelay());
         audioSource.Play();
     }
+    private IEnumerator PlayVoiceWithDelay()
+        {
+            yield return new WaitForSeconds(1f);
+            voiceAudioSource.Play();
+        }
+    
     public void PlayLoseTheme() {
         if (loseTheme == null) Debug.LogWarning("Lose Theme not set in Audio Manager on GameManager");
+        audioSource.Stop();
+        audioSource.loop = false;
         audioSource.clip = loseTheme;
         audioSource.Play();
     }
     public void PlayMainTheme() {
         if (mainTheme == null) Debug.LogWarning("Main Theme not set in Audio Manager on GameManager");
+        audioSource.Stop();
+        audioSource.loop = true;
         audioSource.clip = mainTheme;
         audioSource.Play();
     }
