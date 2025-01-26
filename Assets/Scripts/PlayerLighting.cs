a138193dbb40188d4bfbac609c8362d810bd8fd8
LukeZekes <zechesl@gmail.com>
2025-01-25 23:23:26 -0600

    Gradient lighting

---
+++ Assets/Scripts/PlayerLighting.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerLighting : MonoBehaviour
+{
+    private int textureSize;
+    private float levelHeight = 110f;
+    [SerializeField] private Texture2D gradientTexture;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        textureSize = gradientTexture.height;
+    }
+
+    void Update()
+    {
+        float normalizedPosition = (transform.position.y + 55)/ levelHeight;
+        spriteRenderer.color = SampleGradient(normalizedPosition);
+    }
+
+    public Color SampleGradient(float position)
+    {
+        position = Mathf.Clamp01(position);
+        Debug.Log(position);
+        int pixelPosition = Mathf.RoundToInt(position * (textureSize - 1));
+
+        return gradientTexture.GetPixel(0, pixelPosition);
+    }
+}
