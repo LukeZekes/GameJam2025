e234014253913924ebe044c737c76b72d163fe8b
KitCatLion <91389279+KitCatLion@users.noreply.github.com>
2025-01-26 01:33:45 -0500

    Working Start Menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
+    private int next;
+    void Start()
+    {
+        next = SceneManager.GetActiveScene().buildIndex + 1;
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            SceneManager.LoadScene(next);
         }
     }
 }
