9152f219190d07b7222fce3d2e930b757ff5f47e
Vedasri Malatker <vedasri.malatker@gmail.com>
2025-01-25 21:15:06 -0500

    Update CharaANIMEManager.cs

--- Assets/Scripts/CharaANIMEManager.cs
+++ Assets/Scripts/CharaANIMEManager.cs
@@ -13,11 +13,40 @@ public class CharaANIMEManager : MonoBehaviour
     void Start()
     {
         charaManager = GetComponent<Animator>();
+
+        jump = InputSystem.actions.FindAction("Jump");
+        move = InputSystem.actions.FindAction("Move");
+        dash = InputSystem.actions.FindAction("Sprint");
+
+        charaManager.SetBool("isMove", true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (PlayerAttack.)
+        //if (MOVE)
+        {
+            charaManager.SetBool("isMove", true);
+        }
+
+
+
+        //if (DASH)
+        {
+            //charaManager.SetTrigger("isMove", true);
+        }
+
+        //if (JUMP)
+        {
+
+        }
+
+        //if (Attack)
+        {
+
+        }
+
+        //if (
     }
 }
