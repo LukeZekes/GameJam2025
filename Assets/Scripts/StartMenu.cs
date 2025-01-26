ece2b31b9c5e87f6c10211334b8467931d977d13
KitCatLion <91389279+KitCatLion@users.noreply.github.com>
2025-01-26 01:19:11 -0500

    Menu Title

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
